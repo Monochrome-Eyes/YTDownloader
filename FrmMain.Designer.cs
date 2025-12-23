7d1c66bdf503c753d4e0f9dc0de4b187acffd969
Samuel Shivy <sshivy7@gmail.com>
2025-12-23 16:05:36 -0500

    feat: download single video

---
+++ FrmMain.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace YTDownloader {
+    partial class FrmMain {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.urlLabel = new System.Windows.Forms.Label();
+            this.urlTxtBox = new System.Windows.Forms.TextBox();
+            this.downloadBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // urlLabel
+            // 
+            this.urlLabel.AutoSize = true;
+            this.urlLabel.Location = new System.Drawing.Point(12, 47);
+            this.urlLabel.Name = "urlLabel";
+            this.urlLabel.Size = new System.Drawing.Size(104, 13);
+            this.urlLabel.TabIndex = 0;
+            this.urlLabel.Text = "Enter YouTube URL";
+            // 
+            // urlTxtBox
+            // 
+            this.urlTxtBox.Location = new System.Drawing.Point(12, 63);
+            this.urlTxtBox.Name = "urlTxtBox";
+            this.urlTxtBox.Size = new System.Drawing.Size(409, 20);
+            this.urlTxtBox.TabIndex = 1;
+            // 
+            // downloadBtn
+            // 
+            this.downloadBtn.Location = new System.Drawing.Point(12, 89);
+            this.downloadBtn.Name = "downloadBtn";
+            this.downloadBtn.Size = new System.Drawing.Size(409, 56);
+            this.downloadBtn.TabIndex = 2;
+            this.downloadBtn.Text = "Download Audio";
+            this.downloadBtn.UseVisualStyleBackColor = true;
+            this.downloadBtn.Click += new System.EventHandler(this.downloadBtn_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 184);
+            this.Controls.Add(this.downloadBtn);
+            this.Controls.Add(this.urlTxtBox);
+            this.Controls.Add(this.urlLabel);
+            this.Name = "FrmMain";
+            this.Text = "YouTube Downloader";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label urlLabel;
+        private System.Windows.Forms.TextBox urlTxtBox;
+        private System.Windows.Forms.Button downloadBtn;
+    }
+}
+
---
+++ FrmMain.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YTDownloader {
+    public partial class FrmMain : Form {
+        public FrmMain() {
+            InitializeComponent();
+        }
+
+        private void FrmMain_Load(object sender, EventArgs e) {
+
+        }
+
+        private async void downloadBtn_Click(object sender, EventArgs e) {
+            string urlText = urlTxtBox.Text;
+            if (string.IsNullOrEmpty(urlText)) {
+                MessageBox.Show("Please enter a URL.");
+            }
+
+            try {
+                string musicFilePath = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+                Console.WriteLine(musicFilePath);
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+
+                startInfo.FileName = "C:\\Users\\Heatec\\AppData\\Local\\Programs\\yt-dlp\\yt-dlp.exe";
+                //startInfo.UseShellExecute = false;
+                startInfo.Arguments = $"-x --audio-format mp3 --no-playlist -o {musicFilePath}\\%(title)s.%(ext)s {urlText}";
+
+                await Task.Run(() => {
+                    Process.Start(startInfo);
+                });
+            } catch (Exception ex) {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
