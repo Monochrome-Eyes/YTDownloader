5ff0e30ad3f189b59508a44967a3d4b4fb373118
Samuel Shivy <sshivy7@gmail.com>
2025-12-26 00:05:53 -0500

    publish v1.0.0.2

--- src/FrmMain.cs
+++ src/FrmMain.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -11,7 +12,6 @@ namespace YTDownloader {
         }
 
         private void FrmMain_Load(object sender, EventArgs e) {
-
         }
 
         private async void downloadBtn_Click(object sender, EventArgs e) {
